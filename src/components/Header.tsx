01e5cfeb659935a08ef086f21a3798e10827c3eb
MohammedKaif037 <kaifmohammed037@gmail.com>
2024-12-11 17:08:26 +0530

    refactoring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
             <Code2 className="h-6 w-6 text-indigo-600" />
-            <span className="text-xl font-bold text-gray-800">Mohammed Kaif PortFolio</span>
+            <span className="text-xl font-bold text-gray-800">My PortFolio</span>
           </div>
           
           {/* Desktop Navigation */}
