ef02e454e472a7ce0c6dd2addbd02f5382a96eae
MohammedKaif037 <kaifmohammed037@gmail.com>
2024-12-11 17:03:54 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './index.css';  
 
 function App() {
   return (
